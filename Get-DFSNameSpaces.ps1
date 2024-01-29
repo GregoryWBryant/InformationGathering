897b7ddd49e089940fba9e374485a83a01d579c0
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-01-29 15:59:11 -0500

    Add files via upload

---
+++ Get-DFSNameSpaces.ps1
@@ -0,0 +1,41 @@
+﻿<#
+Script is useful for gathering Information on DFS Shares
+Must have DFS role installed where this is ran
+Creates a Folder on your desktop
+Queries DFS for all names Spaces in current Domain
+Creates a cleaned up export and exports to the new folder
+#>
+
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+$SavePath = $DesktopPath + "\Information Gathered\"
+if(!(Test-Path -Path $SavePath)) {
+
+    New-Item -Path $SavePath -ItemType Directory -Force
+
+}
+
+$NameSpaces = Get-DfsnRoot
+$DFSShares = @()
+
+Foreach ($NameSpace in $NameSpaces) {
+
+    $Folders = Get-DfsnFolder -Path ($NameSpace.Path + "\*")
+    Foreach ($Folder in $Folders) {
+    
+        $Targets = Get-DfsnFolderTarget -Path $Folder.Path
+
+        foreach ($Target in $Targets) {
+            $DFSShare = [PSCustomObject]@{
+                DFSNameSpace = $NameSpace.Path
+                DFSFolder = ($Folder.Path -split '\\')[-1]
+                Target = $Target.TargetPath
+                State = $Target.State
+            }
+            $DFSShares += $DFSShare
+        }
+
+    }
+
+}
+
+$DFSShares | Export-Csv -Path ($SavePath + "DFSShares.csv") -NoTypeInformation
