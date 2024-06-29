17cdf2c991ccc42bd2165cb0cdef48cff38401da
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-06-29 00:46:13 -0400

    Update Get Active Workstations and servers.ps1

--- Get Active Workstations and servers.ps1
+++ Get Active Workstations and servers.ps1
@@ -1,22 +1,43 @@
 <#
-Script is useful for gathering how many Servers and Workstations are active in the last 60 days in Active Directory
-Creates a Folder on your desktop
-Queries Active Directory for all enabled Servers and Workstations
-Creates a cleaned up export and exports to the new folder
+    Script: Active Directory Server and Workstation Activity Report
+
+    Purpose
+        Gathers information about active servers and workstations in the last 60 days.
+        Creates a folder on your desktop to store the results.
+        Exports the information to separate CSV files.
+
+    Requirements:
+        Active Directory PowerShell module (Import-Module ActiveDirectory)
+        Sufficient permissions to query Active Directory computer objects
+
+    Notes
+        Adjust the `$time` variable to modify the time frame for active computers.
+        Customize the `Select` statement to include other relevant properties if needed.
 #>
 
+# Get the path to the user's desktop
 $DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+# Create the save path for the CSV files
 $SavePath = $DesktopPath + "\Information Gathered\"
+
+# Check if the folder exists, create it if not
 if(!(Test-Path -Path $SavePath)) {
-
     New-Item -Path $SavePath -ItemType Directory -Force
-
 }
 
+# Import the Active Directory PowerShell module
 Import-Module ActiveDirectory
 
-$time = (Get-Date).Adddays(-(60))
+# Calculate the date 60 days ago from the current date
+$time = (Get-Date).AddDays(-60)
 
-Get-ADComputer -Filter {LastLogon -gt $time -and enabled -eq $true -and OperatingSystem -like '*Windows Server*'} -Properties * | Select Name,Lastlogondate,OperatingSystem | Export-Csv ($SavePath + "Servers.csv") -NoTypeInformation
+# Get active servers (LastLogonDate within the last 60 days and enabled)
+Get-ADComputer -Filter {LastLogonDate -gt $time -and Enabled -eq $true -and OperatingSystem -like '*Windows Server*'} -Properties * |
+    Select Name,LastLogonDate,OperatingSystem | 
+    Export-Csv ($SavePath + "ActiveServers.csv") -NoTypeInformation
 
-Get-ADComputer -Filter {LastLogon -gt $time -and enabled -eq $true -and OperatingSystem -notlike '*Windows Server*'} -Properties * | Select Name,Lastlogondate,OperatingSystem | Export-Csv ($SavePath + "Workstations.csv") -NoTypeInformation
+# Get active workstations (LastLogonDate within the last 60 days, enabled, and not servers)
+Get-ADComputer -Filter {LastLogonDate -gt $time -and Enabled -eq $true -and OperatingSystem -notlike '*Windows Server*'} -Properties * |
+    Select Name,LastLogonDate,OperatingSystem | 
+    Export-Csv ($SavePath + "ActiveWorkstations.csv") -NoTypeInformation
