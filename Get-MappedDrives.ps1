ab85da3b4c87e5240e65622ae188237744167888
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-06-29 01:07:55 -0400

    Update Get-MappedDrives.ps1
    Commenting

--- Get-MappedDrives.ps1
+++ Get-MappedDrives.ps1
@@ -1,24 +1,49 @@
-﻿<#
-Before running this script you need to setup a Shared Folder that Authenticated users can write to.
-You need to run this script on the Computers with either System or a Local Admin
-This will query the Registry to get all Mapped drives for all users.
+<#
+    Script: Mapped Drive Report
+
+    Purpose
+        Gathers information about mapped drives on the local computer.
+        Exports the information to a CSV file (AllMappedDrives.csv) on a network share.
+
+    Requirements
+        Must be run with System or Local Administrator privileges.
+        A shared folder (\\RS6\MappedDrives in this example) must exist and be accessible to authenticated users with write permissions.
+
+    Note
+        This script only retrieves mapped drives for users whose registry hives are currently loaded (i.e., users who are logged in or whose profiles are actively loaded). Mapped drives for other users will not be detected.
+
+    Usage
+        Run this script on each computer where you want to collect mapped drive information.
+        The results will be appended to the AllMappedDrives.csv file on the \\RS6\MappedDrives share.
 #>
+
+# Get properties of all mapped drives from the registry
 $Drives = Get-ItemProperty "Registry::HKEY_USERS\*\Network\*"
+
+# Initialize an array to store mapped drive information
 $AllMappedDrives = @()
 
+# Iterate through each drive found in the registry
 foreach ($Drive in $Drives) {
 
+    # Extract the user's SID from the registry path
     $Path = $Drive.PSParentPath
-    $Path = $Path.Substring(($Path.IndexOf("\") +1), ($Path.lastIndexOf("\") - $Path.IndexOf("\")))
+    $Path = $Path.Substring(($Path.IndexOf("\") + 1), ($Path.LastIndexOf("\") - $Path.IndexOf("\")))
+
+    # Attempt to get the username associated with the SID (only possible if the user's profile is loaded)
     $Username = (Get-ItemProperty -Path ($Path + "Volatile Environment") -Name "USERNAME").USERNAME
+
+    # Create a custom object to store information about the mapped drive
     $MappedDrive = [PSCustomObject] @{
         "Computer Name" = $env:COMPUTERNAME
         "Drive Letter" = $Drive.PSChildName
         "Drive Location" = $Drive.RemotePath
-        "User Name" = $Username
+        "User Name" = $Username  # This might be empty if the user's profile isn't loaded
     }
-    $AllMappedDrives += $MappedDrive
 
+    # Add the mapped drive object to the array
+    $AllMappedDrives += $MappedDrive
 }
 
-$AllMappedDrives | Export-Csv -Path ("\\ShareServer\MappedDrives\AllMappedDrives.csv") -NoTypeInformation -Append
+# Export the mapped drive information to a CSV file on the network share
+$AllMappedDrives | Export-Csv -Path "\\RS6\MappedDrives\AllMappedDrives.csv" -NoTypeInformation -Append
