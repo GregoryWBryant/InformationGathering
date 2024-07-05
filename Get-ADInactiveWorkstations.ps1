7f03b4fe186e949f15922db878945b514b7a74eb
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-07-05 13:12:21 -0400

    Add files via upload

---
+++ Get-ADInactiveWorkstations.ps1
@@ -0,0 +1,49 @@
+﻿<#
+    .SYNOPSIS
+    Retrieves a list of inactive workstations from Active Directory.
+
+    .DESCRIPTION
+    This script identifies inactive workstations in Active Directory that have been logged into within the last specified number of days. 
+    The workstations must not be Windows Server machines. 
+    The results are saved to a CSV file on the user's desktop.
+
+    .PARAMETER Days
+    The number of days to look back from the current date for the last logon time. 
+    Default is 60 days.
+
+    .EXAMPLE
+    PS C:\> Get-ADInactiveWorkstations -Days 90
+    Retrieves a list of inactive workstations that have been logged into within the last 90 days 
+    and saves the information to a CSV file on the desktop.
+
+    .NOTES
+    The script requires the Active Directory module.
+#>
+
+function Get-ADInactiveWorkstations {
+    param (
+        [int]$Days = 60
+    )
+
+    # Get the path to the user's desktop
+    $DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+    # Create the save path for the CSV files
+    $SavePath = $DesktopPath + "\Information Gathered\"
+
+    # Check if the folder exists, create it if not
+    if(!(Test-Path -Path $SavePath)) {
+        New-Item -Path $SavePath -ItemType Directory -Force
+    }
+
+    # Import the Active Directory PowerShell module
+    Import-Module ActiveDirectory
+
+    # Calculate the date for the specified number of days ago
+    $time = (Get-Date).AddDays(-$Days)
+
+    # Get inactive workstations (LastLogonDate older than the specified time and not running a Windows Server OS)
+    Get-ADComputer -Filter {LastLogonDate -lt $time -and OperatingSystem -notlike '*Windows Server*'} -Properties * |
+        Select-Object Name,LastLogonDate,OperatingSystem | 
+        Export-Csv ($SavePath + "InactiveWorkstations.csv") -NoTypeInformation
+}
