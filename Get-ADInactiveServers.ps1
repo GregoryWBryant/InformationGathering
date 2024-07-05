a7f986a4afeaa278e025a9fcf66c737825948f17
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-07-05 14:27:50 -0400

    Update Get-ADInactiveServers.ps1
    Formatting

--- Get-ADInactiveServers.ps1
+++ Get-ADInactiveServers.ps1
@@ -1,26 +1,26 @@
-<#
-    .SYNOPSIS
-    Retrieves a list of inactive servers from Active Directory.
-
-    .DESCRIPTION
-    This script identifies inactive servers in Active Directory that have not been logged into within the last specified number of days. 
-    The servers must be not be Windows workstation machines. 
-    The results are saved to a CSV file on the user's desktop.
-
-    .PARAMETER Days
-    The number of days to look back from the current date for the last logon time. 
-    Default is 60 days.
-
-    .EXAMPLE
-    PS C:\> Get-ADInactiveServers -Days 90
-    Retrieves a list of inactive servers that have not been logged into within the last 90 days 
-    and saves the information to a CSV file on the desktop.
-
-    .NOTES
-    The script requires the Active Directory module.
-#>
-
 function Get-ADInactiveServers {
+    <#
+        .SYNOPSIS
+            Retrieves a list of inactive servers from Active Directory.
+
+        .DESCRIPTION
+            This script identifies inactive servers in Active Directory that have not been logged into within the last specified number of days. 
+            The servers must be not be Windows workstation machines. 
+            The results are saved to a CSV file on the user's desktop.
+
+        .PARAMETER Days
+            The number of days to look back from the current date for the last logon time. 
+            Default is 60 days.
+
+        .EXAMPLE
+            Get-ADInactiveServers -Days 90
+            Retrieves a list of inactive servers that have not been logged into within the last 90 days 
+            and saves the information to a CSV file on the desktop.
+
+        .NOTES
+            The script requires the Active Directory module.
+    #>
+
     param (
         [int]$Days = 60
     )
