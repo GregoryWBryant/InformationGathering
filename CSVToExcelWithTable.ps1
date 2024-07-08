b1961e5b0b7af66d114cd7d34cd11adc0e3e3fac
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-07-08 15:03:16 -0400

    Add files via upload
    Small script to convert all the CSVs into Excel worksheets and put the Data into a table.

---
+++ CSVToExcelWithTable.ps1
@@ -0,0 +1,16 @@
+﻿# Import the ImportExcel module
+# Install-Module ImportExcel
+# Import-Module ImportExcel
+
+$Files = Get-ChildItem -Path "Path to CSVs"
+
+foreach ($File in $Files) {
+
+    $CSVPath = $File.FullName
+    $XlsxPath = $File.FullName -replace ".csv",".xlsx"
+
+    $Data = Import-Csv -Path $CSVPath
+
+    # Export the data to an XLSX file and format it as a table
+    $Data | Export-Excel -Path $XlsxPath -TableName "DataTable" -AutoSize
+}
